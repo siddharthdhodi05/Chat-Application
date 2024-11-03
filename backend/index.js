37c1926cec0c18d6eda83d1c0ec19d20c1e1fd0d
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-11-04 01:44:53 +0530

    created userroutes, controller and middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config({})
 
 const app = express();
 
-dotenv.config({})
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+
 const PORT = process.env.PORT
+
+
+//routes
+app.use("/api/v1/user", userRoute);
+//http://localhost:8080/api/v1/user/register
+
 
 app.listen( PORT, ()=>{
   connectDB();
